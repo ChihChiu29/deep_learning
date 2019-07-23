3dff69e498d4275ef975751c74a23eb8d9a5ff02
Chih Chiu <chih.chiu.19@gmail.com>
2019-07-22 17:35:41 -0700

    save before batch refactoring

--- experimental/qfunc_multihead.py
+++ experimental/qfunc_multihead.py
@@ -3,8 +3,10 @@ from deep_learning.engine import q_base
 from deep_learning.engine.q_base import States, QValues
 
 
+# TODO: Continue after DQN base class is implemented, after state refactoring
+# is done.
 class MultiHeadQFunction(q_base.QFunction):
-  
+
   def _protected_GetValues(self, states: States) -> QValues:
     pass
 
