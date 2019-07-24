6cd038f5e1d4ecb896fd99c0857358b4c6d28ddf
Chih Chiu <chih.chiu.19@gmail.com>
2019-07-24 16:57:46 -0700

    $@

--- engine/policy_impl.py
+++ engine/policy_impl.py
@@ -62,3 +62,63 @@ class GreedyPolicyWithRandomness(q_base.Policy):
         episode_idx=episode_idx,
         num_of_episodes=num_of_episodes,
       )
+
+
+class GreedyPolicyWithDecreasingRandomness(q_base.Policy):
+  """A policy that almost always the action that gives the max Q value.
+
+  The possibility that it does not perform the best action decreases over steps.
+  """
+
+  def __init__(
+      self,
+      initial_epsilon: float,
+      final_epsilon: float,
+      decay_factor: float,
+  ):
+    """Constructor.
+
+    Args:
+      initial_epsilon: a number between 0 and 1. It gives the probability that a
+        random non-greedy action is chosen initially.
+      final_epsilon: a number between 0 and 1. It gives the probability that a
+        random non-greedy action is chosen eventually.
+      decay_factor: any float number. The epsilon that is actually used
+        is calculated as:
+          e = final_e + (initial_e - final_e) * exp(-decay_factor * episodes)
+    """
+    self._initial_epsilon = initial_epsilon
+    self._final_epsilon = final_epsilon
+    self._decay_factor = decay_factor
+
+    self._num_of_steps = 0
+    self._greedy_policy = GreedyPolicy()
+
+  def Decide(
+      self,
+      env: q_base.Environment,
+      qfunc: q_base.QFunction,
+      state: q_base.State,
+      episode_idx: int,
+      num_of_episodes: int,
+  ) -> q_base.Action:
+    # Calculating e using exp instead of calculating it incrementally because
+    # there is no guarantee if the policy is used in a new run.
+    e = (
+        self._initial_epsilon +
+        (self._initial_epsilon - self._final_epsilon) * numpy.exp(
+      - self._decay_factor * episode_idx))
+    if numpy.random.uniform(0, 1) < e:
+      choice = env.GetRandomChoice()
+      logging.vlog(
+        7, 'making random decision (current e: %f) for state %s choice: %d',
+        e, state, choice)
+      return env.GetActionFromChoice(choice)
+    else:
+      return self._greedy_policy.Decide(
+        env=env,
+        qfunc=qfunc,
+        state=state,
+        episode_idx=episode_idx,
+        num_of_episodes=num_of_episodes,
+      )
--- engine/policy_impl_test.py
+++ engine/policy_impl_test.py
@@ -64,3 +64,43 @@ class PolicyTest(numpy_util_test.NumpyTestCase):
     num_of_0s = len([c for c in choices if c == 0])
     self.assertGreater(num_of_0s, 200)
     self.assertLess(num_of_0s, 300)
+
+  def test_GreedyPolicyWithDecreasingRandomness_epsilonDecays(self):
+    mock_qfunc = mock.MagicMock()
+    mock_qfunc.GetValues.return_value = numpy.array([[0.3, 0.7]])
+
+    env = environment_impl.SingleStateEnvironment(
+      action_space_size=2, step_limit=10)
+    policy = policy_impl.GreedyPolicyWithDecreasingRandomness(
+      initial_epsilon=1.0,
+      final_epsilon=0.0,
+      decay_factor=1.0)
+
+    # When episode_idx is 0, the action should be purely random.
+    choices = []
+    for _ in range(500):
+      choices.append(env.GetChoiceFromAction(policy.Decide(
+        env=env,
+        qfunc=mock_qfunc,
+        state=numpy.array([[0]]),
+        episode_idx=0,
+        num_of_episodes=500)))
+
+    # Tests that roughly half of the time action 0 is chosen.
+    num_of_0s = len([c for c in choices if c == 0])
+    self.assertGreater(num_of_0s, 200)
+    self.assertLess(num_of_0s, 300)
+
+    # When episode_idx is really large, the action should be close to optimal.
+    choices = []
+    for _ in range(500):
+      choices.append(env.GetChoiceFromAction(policy.Decide(
+        env=env,
+        qfunc=mock_qfunc,
+        state=numpy.array([[0]]),
+        episode_idx=50000,
+        num_of_episodes=500)))
+
+    # Tests that almost all the time the optimal action 1 is chosen.
+    num_of_0s = len([c for c in choices if c == 0])
+    self.assertLess(num_of_0s, 5)
